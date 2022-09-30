ff2bafffa402cb1f178125eadec74d1fed7ac33b
user.name <safa.riahi1992@hotmail.com>
2022-09-30 12:58:09 -0700

    size of various type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int itype;
 long int litype;
 long long int llitype;
 printf("Size of char: %zu bytes\n", sizeof(ctype));
-printf("Size of int: %zu bytes\n", sizeof(itype))
+ printf("Size of int: %zu bytes\n", sizeof(itype));
 printf("Size of long int: %zu bytes\n" sizeof(litype));
 printf("Size of long long int: %zu bytes\n" sizeof(llitype));
 return (0);
