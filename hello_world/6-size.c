e66fcf0a4f9582f43d2e35afdaa7c17063e8dd08
user.name <safa.riahi1992@hotmail.com>
2022-09-30 12:52:41 -0700

    size of various type

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - size of various type.
+*Return:0;
+*/
+int main(void)
+{
+char ctype;
+int itype;
+long int litype;
+long long int llitype;
+printf('Size of char: %zu bytes/n', sizeof(ctype));
+printf('Size of int: %zu bytes\n', sizeof(itype))
+printf('Size of long int: %zu bytes\n' sizeof(litype));
+printf('Size of long long int: %zu bytes\n' sizeof(llitype));
+return (0);
+}
