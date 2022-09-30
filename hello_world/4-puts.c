4d14cf4e0d505ac6a3330111098f1e77aa0a796f
user.name <safa.riahi1992@hotmail.com>
2022-09-30 07:07:40 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main puts 
- *Return 0
- *
+/**
+ * main - puts.
+ * Return: int 0
  */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
