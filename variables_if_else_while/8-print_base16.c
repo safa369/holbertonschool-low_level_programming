aaaa1106abcf04453b316367ed120ce063741c89
user.name <safa.riahi1992@hotmail.com>
2022-10-03 07:22:10 -0700

    prints all the numbers of base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers in base 16.
+ * Return:0;
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
