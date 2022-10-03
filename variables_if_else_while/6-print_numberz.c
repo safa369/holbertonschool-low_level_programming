fd487af2ffc47f364e3472920384f366631eb3f3
user.name <safa.riahi1992@hotmail.com>
2022-10-03 06:58:30 -0700

    print numbers from 0 to 9 with putchar and their cod ASCII

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print numbers with putchar function.
+ * Return:0;
+ */
+int main(void)
+{
+int i;
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
