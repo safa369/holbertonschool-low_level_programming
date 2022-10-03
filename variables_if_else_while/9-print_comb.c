48dc0718e5afe1b1fa32a7530a255ca7e521eed1
user.name <safa.riahi1992@hotmail.com>
2022-10-03 08:57:20 -0700

    combination of single digit numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,20 +5,15 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	j = 48;
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i <=57; i++)
 	{
 		putchar(i);
 		if (i < 57)
 		{
-		putchar(' ');
 		putchar(',');
-		}
-		while (j <= 57)
-		{
-			j++;
+		putchar(' ');
 		}
 	}
 		putchar('\n');
