fd2df3aeb421c3da8b9c766a74fec8bd2b3827dd
user.name <safa.riahi1992@hotmail.com>
2022-10-03 05:53:25 -0700

    prints alphabets without the letters e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alphabet.
+ * Return:0;
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
+		{
+			putchar(alph);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
