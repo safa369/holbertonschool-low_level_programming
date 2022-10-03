e7b792384098293c4141458e07de1184f8b6ea2d
user.name <safa.riahi1992@hotmail.com>
2022-10-03 05:16:38 -0700

    prints alphabet in lower case from a to z

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet.
+ * Return:0;
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++ )
+	{
+		putchar(alph);
+	}
+	return(0);
+}
+
