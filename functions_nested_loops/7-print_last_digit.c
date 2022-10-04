fc0f057a7600ae72d1c3de05433de9e2e9d6817c
user.name <safa.riahi1992@hotmail.com>
2022-10-04 13:51:02 -0700

    print the last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+int _putchar(char c);
+/**
+ * print_last_digit - print the las digigt of number.
+ * @n: the number;
+ * Return:number;
+ */
+int print_last_digit(int n)
+{
+	char a;
+
+n = n % 10;
+a = n + 48;
+_putchar(a);
+return (a);
+}
