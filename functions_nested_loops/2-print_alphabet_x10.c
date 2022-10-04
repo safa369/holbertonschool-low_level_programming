44f701a15d34ab13d3039fbba0ca45aeb29eeecc
user.name <safa.riahi1992@hotmail.com>
2022-10-04 04:17:55 -0700

    print alphabets 10times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 int _putchar(char c);
 /**
- * print_alphabet_x10 - print alphabet 10 times;
+ * print_alphabet_x10 - print alphabet 10times;
  * return: void;
  */
 void print_alphabet_x10(void)
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i<= 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for( c = 'a'; c <= 'z'; c++)
 		{
-		_putchar(c);
+			_putchar(c);
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
