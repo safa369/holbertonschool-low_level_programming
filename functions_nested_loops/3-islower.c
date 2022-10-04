3d60fe4fac8328635ab2c08834ed97974730e9d1
user.name <safa.riahi1992@hotmail.com>
2022-10-04 04:48:12 -0700

    function cheks lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_islower - cheks for lowercase character.
+ * Return: 1 or 0;
+ */
+int _islower(int c)
+{
+	char lower;
+
+	if ( islower(c) )
+	{
+		lower = 1;
+	}
+	else
+	{
+		lower = 0;
+	}
+	return lower;
+}
