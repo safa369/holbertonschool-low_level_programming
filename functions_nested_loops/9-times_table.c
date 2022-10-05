5d3a9f4742d2b024fd98035e1b1006859f87cab8
user.name <safa.riahi1992@hotmail.com>
2022-10-05 04:21:33 -0700

    print tabl 10times

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,21 +14,44 @@ void times_table(void)
 			a = i * j;
 			k = a % 10;
 			l = a / 10;
-			if (a < 10)
+			if(a == 0 && j == 0)
 			{
 				_putchar('0' + a);
+				_putchar(',');
+			}
+			else if(j < 9 )
+			{
+				if (a <= 9)
+				{
+					_putchar(' ');	
+					_putchar(' ');
+					_putchar('0' + a);
+					_putchar(',');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar('0' + l);
+					_putchar('0' + k);
+					_putchar(',');
+				}
 			}
 			else
 			{
-				_putchar('0' + l);
-				_putchar('0' + k);
+				if (a <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + a);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar('0' + l);
+					_putchar('0' + k);
+				}
 			}
-			if (j < 9)
-			{
-				_putchar(' ');
-				_putchar(',');
 			}
-		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
