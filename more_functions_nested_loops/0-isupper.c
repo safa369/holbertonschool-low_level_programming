506b0ebba4d51bcc5fecd271168d4fb118bedb43
user.name <safa.riahi1992@hotmail.com>
2022-10-06 06:48:35 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - cheks for uppercase chracter.
+ * @c: inetger.
+ * Return:0 or 1.
+ */
+int _isupper(int c)
+{
+	int r;
+
+	if (c >= 65 && c <= 90)
+	       r = 1;
+	else
+		r = 0;
+	return(r);
+}
+
