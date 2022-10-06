a8bffe801668f0be30c3e3f672883b4c462cb8c8
user.name <safa.riahi1992@hotmail.com>
2022-10-06 07:27:58 -0700

    numbers without 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,15 +4,14 @@
  */
 void print_most_numbers(void)
 {
-        char i;
+	 char i;
 
-        for (i = 48; i < 58; i++)
-        {
+	for (i = 48; i < 58; i++)
+	{
 		if (i != 50 && i != 52)
 		{
-                _putchar(i);
+		_putchar(i);
 		}
-        }
-        _putchar('\n');
+	}
+	_putchar('\n');
 }
-
