ca57cfc3c8a8996c3f99bf4c5d197f12f197eb12
user.name <safa.riahi1992@hotmail.com>
2022-10-06 15:08:18 -0700

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - function that print square.
+ * @size :integer;
+ */
+void print_square(int size)
+{
+int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
