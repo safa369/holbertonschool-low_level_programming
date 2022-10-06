c32d9a4ac80b3bde8885a7f7f2f497bf56b49da9
user.name <safa.riahi1992@hotmail.com>
2022-10-06 07:25:58 -0700

    numbers without 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_most_numbers - function print number from 0 to 9;
+ */
+void print_most_numbers(void)
+{
+        char i;
+
+        for (i = 48; i < 58; i++)
+        {
+		if (i != 50 && i != 52)
+		{
+                _putchar(i);
+		}
+        }
+        _putchar('\n');
+}
+
