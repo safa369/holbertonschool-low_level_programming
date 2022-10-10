28139ed38dd44caf56ce6beb899889e5f00d4922
user.name <safa.riahi1992@hotmail.com>
2022-10-10 04:52:54 -0700

    a half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *puts_half - print half of string;
+ *@str: string);
+*/
+void puts_half(char *str)
+{
+	int a, i, b;
+
+	a = strlen(str);
+	b = a / 2;
+	if (b % 2 == 0)
+	{
+		for (i = b - 1; i < a; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = b; i < a; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
