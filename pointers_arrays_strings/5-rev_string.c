d6cb37646dc06fcd74239151bb25665e426f909c
user.name <safa.riahi1992@hotmail.com>
2022-10-10 04:16:46 -0700

    reverse a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverse string.
+ * @s: char;
+ */
+void rev_string(char *s)
+{
+	int i, len, temp, a;
+	
+	len = strlen(s);
+	a = len / 2;
+	for (i = 0; i < a; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
+
