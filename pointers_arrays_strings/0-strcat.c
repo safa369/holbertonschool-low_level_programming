215573e4e8d203fe11b425c2e672c9c29dd2d25e
user.name <safa.riahi1992@hotmail.com>
2022-10-11 05:11:17 -0700

    concatenate two words

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -7,26 +7,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int  i, k;
-	char str[100];
+	char *c;
+	
+	c = dest;
+	while (*dest != '\0')
+	{
+		*c = *dest;
+		c++;
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*c = *src;
+		c++;
+		src++;
+	}
+	*c = '\0';
 
-	k = 0;
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		str[k] = dest[i];
-		k++;
-		i++;
-	}
-	i = 0;
-	while (src[i] != '\0')
-	{
-		str[k] = src[i];
-		k++;
-		i++;
-	}
-	str[k] = '\0';
-	dest = str;
-	return (dest);
+	return(c);
 }
 
