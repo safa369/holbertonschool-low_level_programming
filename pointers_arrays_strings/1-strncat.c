dd47043d2ef4277efe7908c3fee60d4136d18775
user.name <safa.riahi1992@hotmail.com>
2022-10-11 07:34:53 -0700

    concayenayte two words

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i, a;
 
 	a = strlen(dest);
-	for (i = 0; i <= n-1 && src[i] != '\0'; i++)
+	for (i = 0; i <= n - 1 && src[i] != '\0'; i++)
 	{
 		*(dest + a + i) = *(src + i);
 	}
