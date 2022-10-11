bcd36e715f818e408995d2aa363b95d01232a9ec
user.name <safa.riahi1992@hotmail.com>
2022-10-11 06:48:55 -0700

    concayenayte two words

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two string;
+ *@src: char;
+ *@dest: char;
+ *@n: integer;
+ *Return: char;
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, a;
+
+	a = strlen(dest);
+	i = 0;
+	while (n != 0)
+	{
+		*(dest + a + i) = *src;
+		i++;
+		n--;
+		src++;
+	}
+	return(dest);
+}
