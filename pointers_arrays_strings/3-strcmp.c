348479cd06352474b704f26dfa74d57b88c6734e
user.name <safa.riahi1992@hotmail.com>
2022-10-12 01:59:21 -0700

    compare two string

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,14 @@ int _strcmp(char *s1, char *s2)
 	int i, r;
 
 	i = 0;
-	while(s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
 		i++;
 	}
-	if(s1[i] == '\0' && s2[i] == '\0')
+	if (s1[i] == '\0' && s2[i] == '\0')
 
 		r = 0;
 	else
 		r = *s1 - *s2;
-	return(r);
+	return (r);
 }
