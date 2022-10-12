1e76ee4cb54df03332dd175088487d5e57e14e7f
user.name <safa.riahi1992@hotmail.com>
2022-10-12 03:30:29 -0700

    copye string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i < n && src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 	while (i < n)
 	{
 		dest[i] = '\0';
