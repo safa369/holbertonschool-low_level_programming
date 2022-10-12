5fc5e5782d12fa54009e434d4da5af394b591842
user.name <safa.riahi1992@hotmail.com>
2022-10-12 03:59:00 -0700

    capitalazes string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a sring;
+ * @c: pointer of char;
+ * Return: char;
+ */
+char *cap_string(char *c)
+{
+	int i, n;
+
+	n = strlen(c);
+	i = 0;
+	while (i < n)
+	{
+		if (c[i] != '\\')
+			i++;
+		else if (c[i] == '\\')
+		{
+			if (c[i + 1] == 'n')
+			{
+				c[i] = '\0';
+				i++;
+				c[i] = '\n';
+			}
+			else if (c[i + 1] == 't')
+			{
+			
+				c[i] = '\t';
+				i++;
+			}
+		}
+		i++;
+	}
+	return (c);
+}
