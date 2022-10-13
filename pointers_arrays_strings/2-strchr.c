8fa867a4ec251ecfebccb1958c01c94958e4de67
user.name <safa.riahi1992@hotmail.com>
2022-10-13 11:55:40 -0700

    locate a cha in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strchr - function locate a charactere in a string;
+ * @s: char;
+ * @c: char;
+ * Return: char or NULL;
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		return (s);
+		s++;
+	}
+	return (NULL);
+}
