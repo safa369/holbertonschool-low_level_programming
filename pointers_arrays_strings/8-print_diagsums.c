2902f13f27186547aa1d9792f1396f817d7ca753
user.name <safa.riahi1992@hotmail.com>
2022-10-14 03:27:25 -0700

    print diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,20 +2,20 @@
 /**
  * print_diagsums - print the sum of two diagonals.
  * @a: integer;
- * @size;: integer;
+ * @size: integer;
  */
 void print_diagsums(int *a, int size)
 {
 	int i, sd1, p, sd2;
-	
+
 	sd1 = 0;
 	sd2 = 0;
 	for (i = 0; i < size; i++)
 	{
 		p = (i * size) + i;
 		sd1 = sd1 + *(a + p);
-		p = (i * size) + (size -i - 1);
+		p = (i * size) + (size - i - 1);
 		sd2 = sd2 + *(a + p);
-	 }
+	}
 	printf("%d, %d\n", sd1, sd2);
 }
