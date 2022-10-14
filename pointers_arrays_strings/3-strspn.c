7bab7ea4404de7a665c657251d3e8ecadd1d6137
user.name <safa.riahi1992@hotmail.com>
2022-10-14 13:06:44 -0700

    legnth of a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - function get the lengthnof a prefix.
+ * @s: char.
+ * @accept: char
+ * Return: unsigned int;
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+
+		if (s[i] == accept[j])
+ 		{
+			break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
