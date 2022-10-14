d2a9a2b56d1a8a15deece4fabe0ae23491f4a4b3
user.name <safa.riahi1992@hotmail.com>
2022-10-14 04:28:16 -0700

    locates char in string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-		return (s + i);
+		if (*s == c)
+		{
+			return (s);
+			break;
+		}
+	s++;
 	}
 	return (NULL);
 }
