5203eb02602b76a343d3bb38e2dc0c8b0a83b0f0
user.name <safa.riahi1992@hotmail.com>
2022-10-19 03:59:19 -0700

    header fil

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef INV_TREE_H
+#define INV_TREE_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
