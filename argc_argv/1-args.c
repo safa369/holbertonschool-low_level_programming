2513bb334be8ea2048cc787db67759cf6ba6d798
user.name <safa.riahi1992@hotmail.com>
2022-10-20 02:38:58 -0700

    print number of argument

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - function print the number of argument.
+ * @argc: int;
+ * @argv: char;
+ * Return: always 0.
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
