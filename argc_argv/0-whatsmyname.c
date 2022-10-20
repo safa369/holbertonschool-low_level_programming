08dab155811bcf5b52434e226aa075783808312c
user.name <safa.riahi1992@hotmail.com>
2022-10-20 02:04:41 -0700

    print the name of the program

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * main - function print the name of the program.
+ * @argc: int;
+ * @argv: char;
+ * Return: always 0.
+ */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
