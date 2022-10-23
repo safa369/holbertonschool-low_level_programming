eafba45ffc6e1235c4e69ea7c08ac7e3f57285c5
user.name <safa.riahi1992@hotmail.com>
2022-10-23 08:59:07 -0700

    found if a number is preime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * found_prime - function recursion.
+ * @a: integer.
+ * @b: integer.
+ * return: integer;
+ */
+int found_prime( int a, int b)
+{
+	if (a == b)
+	return(1);
+	if((x % y) != 0)
+		return(0);
+	return (found_prime(x, y+1);
+}
+/**
+ * is_prime_number - function verify if a number is prime.
+ * @n: integer.
+ * Return: 0 or 1;
+ */
+int is_prime_number(int n)
+{
+
+if (n < 2)
+{
+	return(0);
+}
+	return (found-prime(n, 2));
+}
