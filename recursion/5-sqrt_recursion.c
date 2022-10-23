c0aa158618bb4a6a11bab61cfbf7686a099b11d8
user.name <safa.riahi1992@hotmail.com>
2022-10-23 09:27:26 -0700

    sqrt number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * sqrt_main - square number .
+ * @a: integer;
+ * @b: integer;
+ * Return: integer;
+ */
+int sqrt_main(int a, int b)
+{
+	if (a == (b * b))
+		return (b);
+	else if (a > (b * b))
+		return (sqrt_main(a, b + 1))
+	else
+		return (-1);
+}
+/**
+ * _sqrt_recursion - function return the square root.
+ * @n: integer;
+ * Return: integer;
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	else if ( n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (sqrt_main(n, 1));
+}
