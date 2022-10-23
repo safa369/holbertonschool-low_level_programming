c1b9a596b878845769d4a3f5d744184f2761b9a5
user.name <safa.riahi1992@hotmail.com>
2022-10-23 09:02:27 -0700

    found if a number is preime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,13 +5,13 @@
  * @b: integer.
  * return: integer;
  */
-int found_prime( int a, int b)
+int found_prime(int a, int b)
 {
 	if (a == b)
-	return(1);
-	if((x % y) != 0)
-		return(0);
-	return (found_prime(x, y+1);
+	return (1);
+	if((a % b) != 0)
+		return (0);
+	return (found_prime(a, b + 1);
 }
 /**
  * is_prime_number - function verify if a number is prime.
@@ -23,7 +23,7 @@ int is_prime_number(int n)
 
 if (n < 2)
 {
-	return(0);
+	return (0);
 }
 	return (found-prime(n, 2));
 }
