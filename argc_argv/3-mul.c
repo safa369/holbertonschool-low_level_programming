858be5deeec26f06a3445b863e0cb1296b8e9fb9
user.name <safa.riahi1992@hotmail.com>
2022-10-23 08:52:53 -0700

    multipli two arguments

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,15 +10,15 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if (argc < 2)
+	if (argc <= 2)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		a = strtol(argv[1], NULL, 10);
-		b = strtol(argv[2], NULL, 10);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
 		printf("%d\n", a * b);
 	}
 	return (0);
