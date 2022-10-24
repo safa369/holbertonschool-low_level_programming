716eef8a1b4fad116e15a6759e7d28edd826597f
user.name <safa.riahi1992@hotmail.com>
2022-10-24 03:10:01 -0700

    pointer of 2 dimensionnal

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * alloc_grid - func return a pointer to adimen array of integer.
+ * @width: integer.
+ * @height: integer;
+ * Return: integer;
+ */
+int **alloc_grid(int width, int height)
+{
+	int *grid, w, h;
+
+	if ((width <= 0) || (height <= 0))
+		return (NULL);
+	grid = malloc((width * height) * sizeof(*grid));
+	if (grid == NULL)
+		return(NULL);
+	else
+	{
+	w = 0;
+	h = 0;
+	while (w < width)
+	{
+		while (h < height)
+		{
+			*grid[w][h] = 0;
+			h++;
+		}
+		w++;
+	}
+	}
+	return (grid);
+}
