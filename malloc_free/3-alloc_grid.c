6139ea6d87abf27c1831fe4147bcdf84194f5c55
user.name <safa.riahi1992@hotmail.com>
2022-10-24 03:25:20 -0700

    pointer of 2 dimensionnal

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
-	grid = malloc((width * height) * sizeof(*grid));
+	grid = malloc((width * height) * sizeof(**grid));
 	if (grid == NULL)
 	{
 		free(grid);
