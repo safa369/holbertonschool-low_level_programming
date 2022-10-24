d97da75212346e31da94f9c441aec510c00affc8
user.name <safa.riahi1992@hotmail.com>
2022-10-24 13:12:21 -0700

    comcarenate

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,9 +10,9 @@ char *str_concat(char *s1, char *s2)
 	char *c;
 	unsigned int i, j, a, b;
 
-	if (!s1)
+	if (s1 == NULL)
 	       s1 = "";
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
 	i = 0;
 	j = 0;
