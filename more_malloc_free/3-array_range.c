2bc80ece9e2513de265ee0d201db1d20e7987ff9
user.name <safa.riahi1992@hotmail.com>
2022-10-26 04:46:51 -0700

    array of integers

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc((max - min +1) * sizeof(ptr));
+	ptr = malloc((max - min +1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
 	j = 0;
