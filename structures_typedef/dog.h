c928c38f60ab7d6cc666babe93f57c1cc5025f3d
user.name <safa.riahi1992@hotmail.com>
2022-10-31 14:13:41 -0700

    header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - structure of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: structure of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
