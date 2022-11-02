fc491371c7039f7807a0ead6b4f2b363dd4017fc
user.name <safa.riahi1992@hotmail.com>
2022-11-02 09:00:16 -0700

    frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs.
+ * @d: structure.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
