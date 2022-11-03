deaec1a39eabe7dd329cb121d3a7e2c4a114f586
user.name <safa.riahi1992@hotmail.com>
2022-11-03 10:54:35 -0700

    operator function

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - function that add two integer;
+ * @a: integer;
+ * @b: integer;
+ * Retern: integer;
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that return difference between two integer;
+ * @a: integer;
+ * @b: integer;
+ * Retern: integer;
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that multioli two integer;
+ * @a: integer;
+ * @b: integer;
+ * Retern: integer;
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that devise two integer;
+ * @a: integer;
+ * @b: integer;
+ * Retern: integer;
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_add - function that return the remainder of devision of two integer;
+ * @a: integer;
+ * @b: integer;
+ * Retern: integer;
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
