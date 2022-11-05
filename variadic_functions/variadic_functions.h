6d6600c44531eaa33ca29165831ee48f88dd7a2d
user.name <safa.riahi1992@hotmail.com>
2022-11-05 08:08:21 -0700

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
+#ifndef variadic_functions
+#define variadic_functions
 #include <stdio.h>
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
