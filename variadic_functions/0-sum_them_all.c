70f193442376bbcb3a104d185abe7bb32376cfa0
user.name <safa.riahi1992@hotmail.com>
2022-11-05 08:02:27 -0700

    sum of all param

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include<stdarg.h>
+/**
+ * sum_them_all - fanction that return th sum of all parametrs.
+ * @n: number of arguments;
+ * Return: integer;
+ */
+int sum_the_all(const unsigned int n, ...)
+{
+	unsigned int i, s = 0;
+	va_list nptr;
+
+	if (n == 0)
+		return(s);
+	va_start(nptr, n);
+	for (i = 0; i < n; i++)
+		s = va_arg(nptr, unsigned int);
+	va_end(nptr);
+return (s);
+}
