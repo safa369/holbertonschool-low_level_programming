6e95bf6006ef9dd8a2e6bbe47d1439e116432a70
user.name <safa.riahi1992@hotmail.com>
2022-11-05 08:21:16 -0700

    print numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function print numbers.
+ * @separator: char;
+ * @n: integer;
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list nptr;
+
+	va_start(nptr, n);
+		if (separator == NULL)
+		{
+			for (i = 0; i < n; i++)
+				printf("%d ", va_arg(nptr, unsigned int));
+			printf("\n");
+		}
+		else
+		{
+			for (i = 0; i < n - 1; i++)
+				printf("%d, ", va_arg(nptr, unsigned int));
+			printf("%d\n", va_arg(nptr, unsigned int));
+		}
+}
