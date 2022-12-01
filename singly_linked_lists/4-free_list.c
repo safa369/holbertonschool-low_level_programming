03895ee3179b0cee9324e06e7cf597db36bf23a8
user.name <safa.riahi1992@hotmail.com>
2022-12-01 02:28:14 -0800

    free linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - function that frees a linked list.
+ * @head: list_t.
+ */
+void free_list(list_t *head)
+{ 
+	list_t *last;
+
+	while (head != NULL)
+	{
+		last = head;
+		last->next = head->next;
+		free(last->str);
+		free(last);
+		head = head->next;
+	}
+}
