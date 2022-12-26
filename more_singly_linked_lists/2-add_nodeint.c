bd440d986a59ca6ec46c0914e69b4709ea00b8e7
user.name <safa.riahi1992@hotmail.com>
2022-12-26 04:32:01 -0800

    add new node in the beggining of list

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
-	       	return (NULL);
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	*head = new;
