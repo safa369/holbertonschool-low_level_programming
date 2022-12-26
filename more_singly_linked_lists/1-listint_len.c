fd38ca2f953c9372b5c0ec2cf06d444b7685e6d1
user.name <safa.riahi1992@hotmail.com>
2022-12-26 03:58:54 -0800

    count the number of elements

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - count the numbers of elements in a linkesd list.
+ * @h: linked list.
+ * Return: size_t.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t s = 0;
+
+	while(h)
+	{
+		s++;
+		h = h->next;
+	}
+	return(s);
+}
