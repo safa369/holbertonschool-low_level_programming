7164a9ff47f1bae11cfc05ac8fc1dcf18f2952ed
user.name <safa.riahi1992@hotmail.com>
2022-12-27 03:26:33 -0800

    free a linked list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *  * free_listint - funtion that free a linked list.
+ *   * @head: the of a list.
+ *    */
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
