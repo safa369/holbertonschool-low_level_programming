618d48c4ad112a208775d3af805561563b079adb
user.name <safa.riahi1992@hotmail.com>
2022-12-28 03:58:12 -0800

    convert number frpm bin to uint

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * binary_to_uint(const char *b)
+ * @b: the number to convert .
+ * Return: the converted number or 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	int c;
+
+	if (b == NULL)
+		return(0);
+	for (c = 0; b[c]; c++)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return(0);
+		i = 2 * i + (b[c] - '0');
+	}
+	return (i);
+}
