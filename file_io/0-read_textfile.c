bd645dec8f082417df8773fa3fa4fdf871b49d42
user.name <safa.riahi1992@hotmail.com>
2022-12-29 15:02:46 -0800

    read text file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile- function that read a text file.
+ * @filename: the name of  file to read it.
+ * @letters: the number of leters.
+ * Return: return the number of letters are read from the file.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int fr, n;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return(0);
+	fr = read(fd, buf, letters);
+	if (fr == -1)
+		return (0);
+	n = write(fd, buf, letters);
+	if ( n == -1)
+		return (0);
+	return (n);
+}
