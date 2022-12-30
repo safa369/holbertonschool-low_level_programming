4b552da6c2ef3e4d083d129c58e62497cf8d5561
user.name <safa.riahi1992@hotmail.com>
2022-12-30 10:19:07 -0800

    append text to a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * append_text_to_file - function that appends a text at the end of the end of the file.
+ * append_text_to_file - function that appends a text to the end of the file.
  * @filename: the name of the file.
  * @text_content: the text to add to the file.
  * Return: 1 on success and -1 on failure.
  */
-int append_text_to_file(const char * filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fptr, wr, i;
 
