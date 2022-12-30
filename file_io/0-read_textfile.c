5aec1e455f31493c6cd6b9de4b2744ea14844683
user.name <safa.riahi1992@hotmail.com>
2022-12-30 06:06:57 -0800

    read text file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -18,12 +18,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
-		return(0);
+		return (0);
 	fr = read(fd, buf, letters);
 	if (fr == -1)
 		return (0);
 	n = write(STDOUT_FILENO, buf, fr);
-	if ( n == -1)
+	if (n == -1)
 		return (0);
 	free(buf);
 	close(fd);
