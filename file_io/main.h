c2d6005b020ff5a56371dc4e2d10ae5dd85bca65
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-04 23:16:40 +0100

    header file

--- file_io/main.h
+++ file_io/main.h
@@ -9,4 +9,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void file_err(int r, int er, int fv, char *file);
 #endif
