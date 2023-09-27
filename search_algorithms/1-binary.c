8c83843210f750bf67d4e70d524eb6a7215c6438
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-09-27 19:48:22 +0200

    commit

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * binary_search - unction that searches for a value in a sorted array.
+ * @array: array of intgers.
+ * @size: the size of array.
+ * @value: the value to search it.
+ * Return: index of value foound in array or -1 if not found.
+*/
+int binary_search(int *array, size_t size, int value)
+{
+int middle, i, left = 0, right = size - 1;
+if (array == NULL)
+return (-1);
+while (left <= right)
+{
+middle = ((left + right) / 2);
+printf("Searching in array:");
+for (i = middle; i <= right; i++)
+{
+if (i != right)
+printf(" %d,", array[i]);
+else
+printf(" %d", array[i]);
+}
+printf("\n");
+if (value > array[middle])
+left = middle + 1;
+else if (value < array[middle])
+right  = middle - 1;
+else if (value == array[middle])
+return (middle);
+}
+return (-1);
+}
